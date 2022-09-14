54954aaa63d76a0ffc35182c674e4387e023fa8c
Prakasa2021 <uzumakidef@gmail.com>
2022-09-14 18:52:17 +0700

    Toothbrush mechanism

---
+++ Assets/Scripts/ToothBrush/AlphaChange.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlphaChange : MonoBehaviour
+{
+    SpriteRenderer Sprite;
+
+    private void Start()
+    {
+        Sprite = GetComponent<SpriteRenderer>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        Color currCol = Sprite.color;
+
+        if(currCol.a > 0)
+        {
+            Color newCol = new Color(currCol.r, currCol.g, currCol.b, currCol.a - 0.2f);
+            Sprite.color = newCol;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
