de3a0722917db6c5ae8ffc47f6014296385a6aa3
Hanif <85260485+hxnmi@users.noreply.github.com>
2022-09-15 13:57:56 +0700

    DragnDrop 2 inventory system

---
+++ Assets/Scripts/DragnDrop/CorrectSlot.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CorrectSlot : DragSlot
+{
+	protected override void Awake()
+	{
+		base.Awake();
+		DropArea.DropConditions.Add(new IsCorrectCondition());
+	}
+}
---
+++ Assets/Scripts/DragnDrop/IsCorrectCondition.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class IsCorrectCondition : DropCondition
+{
+	public override bool Check(DraggableComponent draggable)
+	{
+        if (draggable.GetComponent<Correct>() != null)
+        {
+            Console.WriteLine("Benar");
+            Console.ReadKey();
+        }
+        else
+        {
+            Console.WriteLine("SALAH");
+            Console.ReadKey();
+        }
+        return draggable.GetComponent<Correct>() != null;
+    }
+}
