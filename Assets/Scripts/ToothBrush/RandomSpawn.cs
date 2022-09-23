9f7e832a4abed5152ff4607bc118f1224026bd33
Prakasa2021 <uzumakidef@gmail.com>
2022-09-23 16:26:18 +0700

    add all levels

--- Assets/Scripts/ToothBrush/RandomSpawn.cs
+++ Assets/Scripts/ToothBrush/RandomSpawn.cs
@@ -9,7 +9,7 @@ public class RandomSpawn : MonoBehaviour
 
     void Start()
     {
-        for(int i=0; i<8; i++)
+        for(int i=0; i<10; i++)
         {
             int randEnemy = Random.Range(0, enemyPrefs.Length);
             int randSpawn = Random.Range(0, spawnPoints.Length);
