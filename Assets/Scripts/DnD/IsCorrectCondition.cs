7819ed2f7f76832c75e60ea237b6c2305ac12213
Hanif <85260485+hxnmi@users.noreply.github.com>
2022-10-03 08:43:19 +0700

    sort and delete useless assets

--- Assets/Scripts/DnD/IsCorrectCondition.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-public class IsCorrectCondition : DropCondition
-{
-	public override bool Check(DraggableComponent draggable)
-	{
-        return draggable.GetComponent<Correct>() != null;
-    }
-}
