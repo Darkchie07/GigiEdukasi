663c9a39d13bf2a68f628be34763e53ef316ebe2
Darkchie07 <49508444+darkchie@users.noreply.github.com>
2022-10-05 20:34:21 +0700

    Revisi

--- Assets/Scripts/HomeMenu/SceneManagers.cs
+++ Assets/Scripts/HomeMenu/SceneManagers.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class SceneManager : MonoBehaviour
+public class SceneManagers : MonoBehaviour
 {
     public void PindahScene(string _path)
     {
